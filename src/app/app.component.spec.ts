096728c0ad7e6e9e146f3684aea2125182439c6a
ugfire <ugfirecompany@gmail.com>
2024-11-26 08:37:06 +0300

    renamed from ecard to hivtracker

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ecard' title`, () => {
+  it(`should have the 'hivtracker' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ecard');
+    expect(app.title).toEqual('hivtracker');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ecard');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hivtracker');
   });
 });
