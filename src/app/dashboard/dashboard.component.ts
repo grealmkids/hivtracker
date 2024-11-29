c51500afb6fcbd47a492334a5a014c53c86068e9
ugfire <ugfirecompany@gmail.com>
2024-11-29 11:36:42 +0300

    call selected patient alert edit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -228,7 +228,7 @@ labForselectedPatient: any;
     callSelectedPatient(arg0: any) {
            Swal.fire({  
               
-        html: `<p>Update data for <p><p class="text-success"> <strong>${this.selectedPatient.name}</strong></p><h3 class="text-secondary"> TEL : <span class="text-dark"> ${this.selectedPatient.mobile} </span> </h3>`,  
+        html: `<p>Make new Call to <p><p class="text-success"> <strong>${this.selectedPatient.name}</strong></p><h3 class="text-secondary"> TEL : <span class="text-dark"> ${this.selectedPatient.mobile} </span> </h3>`,  
         icon: 'warning',  
         confirmButtonText: 'Okay'  
     }); 
